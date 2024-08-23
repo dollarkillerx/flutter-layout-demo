20bdd35716dd7a5e77126a78203318e3173d5fd5
Леони́д Ильи́ч Бре́жнев <google@gmail.com>
2024-08-23 16:41:05 +0900

    bloc

---
+++ bloc_demo1/android/app/src/main/kotlin/com/bloc_demo1/bloc_demo1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bloc_demo1.bloc_demo1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ bloc_demo1/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/counter_cubit.dart';
+import 'counter_observer.dart';
+
+void main() {
+  Bloc.observer = const CounterObserver();
+  runApp(const CounterApp());
+}
+
+
+/// {@template counter_app}
+/// A [MaterialApp] which sets the `home` to [CounterPage].
+/// {@endtemplate}
+class CounterApp extends MaterialApp {
+  /// {@macro counter_app}
+  const CounterApp({super.key}) : super(home: const CounterPage());
+}
+
+/// {@template counter_page}
+/// A [StatelessWidget] which is responsible for providing a
+/// [CounterCubit] instance to the [CounterView].
+/// {@endtemplate}
+class CounterPage extends StatelessWidget {
+  /// {@macro counter_page}
+  const CounterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => CounterCubit(),
+      child: const CounterView(),
+    );
+  }
+}
+
+/// {@template counter_view}
+/// A [StatelessWidget] which reacts to the provided
+/// [CounterCubit] state and notifies it in response to user input.
+/// {@endtemplate}
+class CounterView extends StatelessWidget {
+  /// {@macro counter_view}
+  const CounterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      body: Center(
+        child: BlocBuilder<CounterCubit, int>(
+          builder: (context, state) {
+            return Text('$state', style: textTheme.displayMedium);
+          },
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            key: const Key('counterView_increment_floatingActionButton'),
+            child: const Icon(Icons.add),
+            onPressed: () => context.read<CounterCubit>().increment(),
+          ),
+          const SizedBox(height: 8),
+          FloatingActionButton(
+            key: const Key('counterView_decrement_floatingActionButton'),
+            child: const Icon(Icons.remove),
+            onPressed: () => context.read<CounterCubit>().decrement(),
+          ),
+        ],
+      ),
+    );
+  }
+}
